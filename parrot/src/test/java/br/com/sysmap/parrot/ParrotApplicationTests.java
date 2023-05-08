394957879e2204f6f90790175199634bc6d4a606
FILIPE SANTOS <RM93042@fiap.com.br>
2023-05-07 22:42:25 -0300

    inicial projeto

---
+++ parrot/src/test/java/br/com/sysmap/parrot/ParrotApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sysmap.parrot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParrotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
